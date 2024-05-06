1cc48458c7bab957e5483696ef50eaaf5643854e
viethungle1 <hunglv0801@gmail.com>
2024-05-06 08:29:27 +0700

    songs

--- src/main/java/com/social_music/controller/SongController.java
+++ src/main/java/com/social_music/controller/SongController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 public class SongController {
     @Autowired
     private SongServiceImpl songService;
+
     @GetMapping()
     public ResponseEntity<Iterable<Song>> findAll() {
         return new ResponseEntity<>(songService.findAll(), HttpStatus.OK);
@@ -25,6 +26,14 @@ public class SongController {
     @PostMapping("/user/create")
     public ResponseEntity<?> create(@RequestBody Song song) {
         return new ResponseEntity<>(songService.save(song),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/count/{id}")
+    public ResponseEntity<?> countListen(@PathVariable Long id) {
+        Optional<Song> songOptional = songService.findById(id);
+        Song song = songOptional.get();
+        song.setListenCount(song.getListenCount() + 1);
+        return new ResponseEntity<>(songService.save(song),HttpStatus.OK);
     }
 
     @PutMapping("/user/update")
@@ -41,5 +50,33 @@ public class SongController {
     public ResponseEntity<?> delete(@PathVariable Long id) {
         songService.remove(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("findSongByTitle/{title}")
+    public ResponseEntity<Iterable<Song>> findSongByTitle(@PathVariable String title) {
+        return new ResponseEntity<>(songService.getSongByTitle(title), HttpStatus.OK);
+    }
+
+    @GetMapping("findSongByAuthor/{author}")
+    public ResponseEntity<Iterable<Song>> findSongByAuthor(@PathVariable String author) {
+        return new ResponseEntity<>(songService.getSongByAuthor(author), HttpStatus.OK);
+    }
+
+    @GetMapping("findSongBySinger/{singer}")
+    public ResponseEntity<Iterable<Song>> findSongBySinger(@PathVariable String singer) {
+        return new ResponseEntity<>(songService.getSongBySinger(singer), HttpStatus.OK);
+    }
+    @GetMapping("newSongsList")
+    public ResponseEntity<Iterable<Song>> newSongsList() {
+        return new ResponseEntity<>(songService.getNewSongsList(),HttpStatus.OK);
+    }
+
+    @GetMapping("hotSongsList")
+    public ResponseEntity<Iterable<Song>> hotSongsList() {
+        return new ResponseEntity<>(songService.getHotSongsList(),HttpStatus.OK);
+    }
+    @GetMapping("favoriteSongs")
+    public ResponseEntity<Iterable<Song>> favoriteSongs() {
+        return new ResponseEntity<>(songService.getFavoriteSongs(),HttpStatus.OK);
     }
 }
